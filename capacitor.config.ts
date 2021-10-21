659ee6fe417d6d5647dc99d4daee065112c2a475
Ford Leins <fordleins@gmail.com>
2021-10-22 12:56:00 +1300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'messenger-app',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
